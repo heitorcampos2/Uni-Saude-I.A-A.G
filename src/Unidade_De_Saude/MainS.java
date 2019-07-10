d3ed2997a96b570ae36d6882a089a9ba508dccc6
heitorcampos2 <heitormtb12@gmail.com>
2019-07-10 00:11:11 -0300

    v3

--- src/Unidade_De_Saude/MainS.java
+++ src/Unidade_De_Saude/MainS.java
@@ -10,7 +10,7 @@ package Unidade_De_Saude;
  * @author HeiThor
  */
 public class MainS {
-    public class void mais(String[] args){
+    public static void main(String[] args){
         Bairro[] bairros = new Bairro[56];
                               //x  y  w
         bairros[0] = new Bairro(0, 0, 7);
@@ -40,7 +40,42 @@ public class MainS {
         bairros[22] = new Bairro(2, 6, 3);
         bairros[23] = new Bairro(2, 7, 9);
         
-        bairros[55] = new Bairro(7, 6, 8);
+        bairros[24] = new Bairro(3, 0, 1);
+        bairros[25] = new Bairro(3, 1, 8);
+        bairros[26] = new Bairro(3, 2, 5);
+        bairros[27] = new Bairro(3, 3, 6);
+        bairros[28] = new Bairro(3, 4, 7);
+        bairros[29] = new Bairro(3, 5, 5);
+        bairros[30] = new Bairro(3, 6, 4);
+        bairros[31] = new Bairro(3, 7, 2);
+        
+        bairros[32] = new Bairro(4, 0, 5);
+        bairros[33] = new Bairro(4, 1, 8);
+        bairros[34] = new Bairro(4, 2, 6);
+        bairros[35] = new Bairro(4, 3, 5);
+        bairros[36] = new Bairro(4, 4, 5);
+        bairros[37] = new Bairro(4, 5, 4);
+        bairros[38] = new Bairro(4, 6, 2);
+        bairros[39] = new Bairro(4, 7, 9);
+        
+        bairros[40] = new Bairro(5, 0, 1);
+        bairros[41] = new Bairro(5, 1, 6);
+        bairros[42] = new Bairro(5, 2, 9);
+        bairros[43] = new Bairro(5, 3, 3);
+        bairros[44] = new Bairro(5, 4, 5);
+        bairros[45] = new Bairro(5, 5, 5);
+        bairros[46] = new Bairro(5, 6, 6);
+        bairros[47] = new Bairro(5, 7, 4);
+        
+        bairros[48] = new Bairro(6, 0, 6);
+        bairros[49] = new Bairro(6, 1, 3);
+        bairros[50] = new Bairro(6, 2, 9);
+        bairros[51] = new Bairro(6, 3, 9);
+        bairros[52] = new Bairro(6, 4, 3);
+        bairros[53] = new Bairro(6, 5, 8);
+        bairros[54] = new Bairro(6, 6, 5);
+        bairros[55] = new Bairro(6, 7, 8);
     }
+    
     
 }
