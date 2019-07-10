b75c835129c89d3d1e0bf08134e660b04cd5862f
heitorcampos2 <heitormtb12@gmail.com>
2019-07-10 00:04:43 -0300

    v1

---
+++ src/Unidade_De_Saude/MainS.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unidade_De_Saude;
+
+/**
+ *
+ * @author HeiThor
+ */
+public class MainS {
+    public class void mais(String[] args){
+        Bairro[] bairros = new Bairro[56];
+                              //x  y  w
+        bairros[0] = new Bairro(0, 0, 7);
+        bairros[1] = new Bairro(0, 1, 5);
+        bairros[2] = new Bairro(0, 2, 2);
+        bairros[3] = new Bairro(0, 3, 5);
+        bairros[4] = new Bairro(0, 4, 5);
+        bairros[5] = new Bairro(0, 5, 5);
+        bairros[6] = new Bairro(0, 6, 4);
+        bairros[7] = new Bairro(0, 7, 5);
+        
+        bairros[8] = new Bairro(1, 0, 1);
+        bairros[9] = new Bairro(1, 1, 4);
+        bairros[10] = new Bairro(1, 2, 2);
+        bairros[11] = new Bairro(1, 3, 8);
+        bairros[12] = new Bairro(1, 4, 9);
+        bairros[13] = new Bairro(1, 5, 1);
+        bairros[14] = new Bairro(1, 6, 1);
+        bairros[15] = new Bairro(1, 7, 8);
+        
+        bairros[16] = new Bairro(2, 0, 3);
+        bairros[17] = new Bairro(2, 1, 5);
+        bairros[18] = new Bairro(2, 2, 5);
+        bairros[19] = new Bairro(2, 3, 1);
+        bairros[20] = new Bairro(2, 4, 7);
+        bairros[21] = new Bairro(2, 5, 2);
+        bairros[22] = new Bairro(2, 6, 3);
+        bairros[23] = new Bairro(2, 7, 9);
+        
+        bairros[55] = new Bairro(7, 6, 8);
+    }
+    
+}
